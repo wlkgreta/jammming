e31d7afb38245e87e5070b40408dd733edcd1a96
wknightly <wknightly22@gmail.com>
2025-02-06 16:32:44 -0600

    Created new TrackList.js file and added to TrackList directory within Components directory

---
+++ src/Components/Tracklist/Tracklist.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Tracklist.css';
+
+function Tracklist() {
+    return (
+        <div className="TrackList">
+            {/* You will add a map method that renders a set of Track components*/}
+        </div>
+    );
+}
+
+export default Tracklist;
