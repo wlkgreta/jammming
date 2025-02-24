f3298822a0c00be9004838bc2615f87784eb1864
wknightly <wknightly22@gmail.com>
2025-02-24 14:15:49 -0600

    updated SearchBar component to include a UseState hook for the search term, as well as functions for handleTermChange, handleSearch, and handleKeyPress (pressing enter handles search).

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './SearchBar.module.css';
 
-function SearchBar() {
+function SearchBar(props) {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    function handleTermChange({target}) {
+        setSearchTerm(target.value);
+    };
+
+    function handleKeyPress(event) {
+        if (event.key === 'Enter') {
+            handleSearch();
+        }
+    };
+
+    function handleSearch() {
+        props.onSearch(searchTerm);
+    };
+
     return (
         <div className={styles.SearchBar}>
-            <input placeholder="Enter A Song, Album, or Artist" />
-            <button className={styles.SearchButton}>SEARCH</button>
+            <input placeholder="Enter A Song, Album, or Artist" onChange={handleTermChange} onKeyDown={handleKeyPress}/>
+            <button className={styles.SearchButton} onClick={handleSearch}>SEARCH</button>
         </div>
     );
 }
