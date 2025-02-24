c55b2be7ea964081de06977ed6739281c4a37dd3
wknightly <wknightly22@gmail.com>
2025-02-24 14:19:31 -0600

    imported "pencil" icon to place next to Playlist Name to help indicate it is something you can edit. Also updated Playlist component to include handleNameChange function.

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import styles from './Playlist.module.css';
 import Tracklist from '../Tracklist/Tracklist';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPencil } from '@fortawesome/free-solid-svg-icons';
 
 function Playlist(props) {
+    function handleNameChange({target}) {
+        props.onNameChange(target.value);
+    };
+    
     return (
         <div className={styles.Playlist}>
-            <input defaultValue={'New Playlist'}/>
-            <Tracklist userSearchResults={props.playlistTracks} />
-            <button className={styles.PlaylistSave}>SAVE TO SPOTIFY</button>
+            <div className={styles.PlaylistName}>
+                <input className={styles.input} value={props.playlistName} onClick={props.onClick} onChange={handleNameChange}/>
+                
+                <FontAwesomeIcon className={styles.icon} icon={faPencil} onClick={props.onClick}/>
+            </div>
+
+            <div className={styles.Tracklist}>
+                <Tracklist userSearchResults={props.playlistTracks} onRemove={props.onRemove} isRemoval={props.isRemoval}/>
+
+                <button className={styles.PlaylistSave} onClick={props.onSave}>SAVE TO SPOTIFY</button>
+            </div>
         </div>
     );
 }
