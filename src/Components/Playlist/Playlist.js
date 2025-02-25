8f1b8fbc852cd8a4bc136027f96eac6de79e855c
wknightly <wknightly22@gmail.com>
2025-02-25 14:04:16 -0600

    Added Popup to render on Save To Spotify click

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Popup from 'reactjs-popup';
 import styles from './Playlist.module.css';
 import Tracklist from '../Tracklist/Tracklist';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -12,7 +13,7 @@ function Playlist(props) {
     return (
         <div className={styles.Playlist}>
             <div className={styles.PlaylistName}>
-                <input className={styles.input} value={props.playlistName} onClick={props.onClick} onChange={handleNameChange}/>
+                <input className={styles.input} value={props.playlistName} onClick={props.onNameClick} onChange={handleNameChange}/>
                 
                 <FontAwesomeIcon className={styles.icon} icon={faPencil} onClick={props.onClick}/>
             </div>
@@ -21,6 +22,16 @@ function Playlist(props) {
                 <Tracklist userSearchResults={props.playlistTracks} onRemove={props.onRemove} isRemoval={props.isRemoval}/>
 
                 <button className={styles.PlaylistSave} onClick={props.onSave}>SAVE TO SPOTIFY</button>
+                
+                <Popup open={props.open}>
+                    <div className={styles.SavePopup}>
+                        <button className={styles.SaveClose} onClick={props.onClose}>
+                            &times;
+                        </button>
+                        <h2>Rock on!</h2>
+                        <p>Your new playlist is saved to Spotify</p>
+                    </div>
+                </Popup>
             </div>
         </div>
     );
